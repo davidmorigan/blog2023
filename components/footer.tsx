d1f758eb6fb83441b37e574d27a55d11c3311f5b
David Mora Gandarillas <davidmorigan@gmail.com>
2022-12-30 22:46:35 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,16 +11,16 @@ const Footer = () => {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
+              href="https://www.linkedin.com/in/davidmorigan/"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               View Linkedin profile
             </a>
             <a
-              href={`https://www.linkedin.com/in/davidmorigan/`}
+              href={`https://www.twitter.com/davidmorigan/`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              View on Twitter
             </a>
           </div>
         </div>
